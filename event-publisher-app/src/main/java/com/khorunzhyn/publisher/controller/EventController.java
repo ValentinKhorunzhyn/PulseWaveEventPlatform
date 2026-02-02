c872c245bfe159bd5bea30f877f7d3f6663058d4
ValentinKhorunzhyn <messuri.87@mail.ru>
2026-02-02 15:02:02 +0300

    Implement observability logging and metrics. Update config files

--- event-publisher-app/src/main/java/com/khorunzhyn/publisher/controller/EventController.java
+++ event-publisher-app/src/main/java/com/khorunzhyn/publisher/controller/EventController.java
@@ -1,16 +1,16 @@
 package com.khorunzhyn.publisher.controller;
 
 import com.khorunzhyn.publisher.model.Event;
+import com.khorunzhyn.publisher.service.EventPublisherService;
 import com.khorunzhyn.publisher.service.EventService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +22,7 @@ import java.util.List;
 public class EventController {
 
     private final EventService eventService;
+    private final EventPublisherService eventPublisherService;
 
     @GetMapping("/recent")
     @Operation(summary = "Get last events")
@@ -30,4 +31,16 @@ public class EventController {
         return ResponseEntity.ok(eventService.getRecentEvents(limit));
     }
 
+    @PostMapping("/generate")
+    @Operation(summary = "Generate scheduled event")
+    public ResponseEntity<Void> generateEvent() {
+        log.info("Generate event from local server for testing");
+
+        log.info("Checking trace context...");
+        var mdcContent = MDC.getCopyOfContextMap();
+        log.info("MDC Content: " + mdcContent);
+
+        eventPublisherService.generateEvent();
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
 }

fce99a59bdbfa71ffad15ae2a19d4a6d3317258e
ValentinKhorunzhyn <messuri.87@mail.ru>
2026-04-22 17:29:35 +0300

    Add authentification and autorization with oauth2 and keycloak

--- event-publisher-app/src/main/java/com/khorunzhyn/publisher/controller/StatisticsController.java
+++ event-publisher-app/src/main/java/com/khorunzhyn/publisher/controller/StatisticsController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class StatisticsController {
     private final StatisticService statisticService;
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @Operation(summary = "Get all statistics by events")
     public ResponseEntity<StatsResponseDto> getStats() {
         StatsResponseDto stats = statisticService.getStatistics();
